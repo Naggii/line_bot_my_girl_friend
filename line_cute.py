e3611accc1941dd21bc0385df3ee119f28adca97
anagai <anagai@sonix.asia>
2021-01-29 01:42:13 +0900

    fix send message

--- line_cute.py
+++ line_cute.py
@@ -12,7 +12,7 @@ line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
 def send_message():
   USER_ID = info['USER_ID']
-  cute_messages = TextSendMessage(text="おはよう、今日も頑張ってね❤️ \n無理しないようにね❤️")
+  cute_messages = TextSendMessage(text="おはよう、あゆむん〜！今日も頑張ってね❤️ \n無理しないようにね❤️")
   line_bot_api.push_message(USER_ID, messages=cute_messages)
 
 
