924e763295fabefc336721d09db5c202ba8721c5
Naggii <amna203090@gmail.com>
2021-04-10 21:00:33 +0900

    オウム返し、webhook

--- line_cute.py
+++ line_cute.py
@@ -1,20 +1,69 @@
+from flask import Flask, request, abort
+ 
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+import os
 import json
 from os import name
 from linebot import LineBotApi
 import linebot
 from linebot.models import TextSendMessage, messages
+ 
+app = Flask(__name__)
+ 
+#環境変数取得
+# LINE Developersで設定されているアクセストークンとChannel Secretをを取得し、設定します。
+YOUR_CHANNEL_ACCESS_TOKEN = info["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = info["YOUR_CHANNEL_SECRET"]
 
-file = open("info.json", "r")
-info = json.load(file)
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+ 
+ 
+## 1 ##
+#Webhookからのリクエストをチェックします。
+@app.route("/callback", methods=['POST'])
+def callback():
+    # リクエストヘッダーから署名検証のための値を取得します。
+    signature = request.headers['X-Line-Signature']
+ 
+    # リクエストボディを取得します。
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+ 
+    try:
+        handler.handle(body, signature)
 
-CHANNEL_ACCESS_TOKEN = info['CHANNEL_ACCESS_TOKEN']
-line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+    except InvalidSignatureError:
+        abort(400)
 
-def send_message():
-  USER_ID = info['USER_ID']
-  cute_messages = TextSendMessage(text="おはよう、あゆむ~！今日も頑張ってね \n無理しないようにね❤️")
-  line_bot_api.push_message(USER_ID, messages=cute_messages)
-
-
+    return 'OK'
+ 
+## 2 ##
+###############################################
+#LINEのメッセージの取得と返信内容の設定(オウム返し)
+###############################################
+ 
+#LINEでMessageEvent（普通のメッセージを送信された場合）が起こった場合に、
+#def以下の関数を実行します。
+#reply_messageの第一引数のevent.reply_tokenは、イベントの応答に用いるトークンです。 
+#第二引数には、linebot.modelsに定義されている返信用のTextSendMessageオブジェクトを渡しています。
+ 
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)) #ここでオウム返しのメッセージを返します。
+ 
+# ポート番号の設定
 if __name__ == "__main__":
-  send_message()
+#    app.run()
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
