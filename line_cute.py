4c7e6d6cc5d793afca619d474d0d442d34ada5d6
Naggii <amna203090@gmail.com>
2021-04-10 21:46:16 +0900

    fa

--- line_cute.py
+++ line_cute.py
@@ -52,10 +52,10 @@ def callback():
  
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if message.text == "ayumu":
+    if event.message.text == "ayumu":
         line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text="fucking idiot"))
+        TextSendMessage(text="Genius !!"))
     else:
         line_bot_api.reply_message(
         event.reply_token,
