b444cacad67536e1942a5214aed60a44fe33d069
Naggii <amna203090@gmail.com>
2021-04-10 21:40:10 +0900

    add anagai reply fucking idiot

--- line_cute.py
+++ line_cute.py
@@ -52,7 +52,12 @@ def callback():
  
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
+    if message == "ayumu":
+        line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text="fucking idiot"))
+    else:
+        line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text)) #ここでオウム返しのメッセージを返します。
  
